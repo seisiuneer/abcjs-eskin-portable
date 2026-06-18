0a465719846f7764907761594637bfd734cf4fe4
Michael Eskin <seisiuneer@gmail.com>
2026-06-18 11:36:27 -0700

    Minor control tooltip change

--- dist/abcjs-eskin-portable.js
+++ dist/abcjs-eskin-portable.js
@@ -11,7 +11,7 @@
  *
  * MIT License
  * 
- * Copyright (c) 2026 Michael Eskin 
+ * Copyright (c) 2026 Michael Eskin  
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
